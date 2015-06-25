7f956fe41213478e04310225b06595e52bcdc9ce
MphoM <mphego@ska.ac.za>
2015-06-25 09:48:09 +0200

    Added zero and non zero calculations.

--- mkat_fpga_tests/test_cbf.py
+++ mkat_fpga_tests/test_cbf.py
@@ -126,7 +126,7 @@ class test_CBF(unittest.TestCase):
         chan_ripple = max_chan_response - min_chan_response
         acceptable_ripple_lt = 0.3
 
-        matplotlib.use('Agg')
+       # matplotlib.use('Agg')
         fig = plt.plot(desired_chan_test_freqs, loggerise(chan_responses[:, test_chan],
                        dynamic_range=60))[0]
         axes = fig.get_axes()
@@ -219,7 +219,6 @@ class test_CBF(unittest.TestCase):
         test_data = self.receiver.get_clean_dump(DUMP_TIMEOUT)['xeng_raw']
         self.assertFalse(nonzero_baselines(test_data))
         #-----------------------------------
-#        input_labels = ['m000_x', 'm000_y', 'm001_x', 'm001_y']
         all_inputs = sorted(set(input_labels))
         zero_inputs = set(input_labels)
         nonzero_inputs = set()
@@ -235,20 +234,18 @@ class test_CBF(unittest.TestCase):
                         zeros.add((inp_i, inp_j))
             return zeros, nonzeros
 
-        zero_baseline, nonzero_baseline = calc_zero_and_nonzero_baselines(nonzero_inputs)
-
+        #zero_baseline, nonzero_baseline = calc_zero_and_nonzero_baselines(nonzero_inputs)
         def print_baselines():
             print ('zeros: {}\n\nnonzeros: {}\n\nnonzero-baselines: {}\n\n '
                 'zero-baselines: {}\n\n'.format(
                     sorted(zero_inputs), sorted(nonzero_inputs),
                     sorted(nonzero_baseline), sorted(zero_baseline)))
-
         #print_baselines()
         for inp in input_labels:
             old_eqs = initial_equalisations[inp]
             self.correlator.feng_eq_set(source_name=inp, new_eq=old_eqs)
             zero_inputs.remove(inp)
             nonzero_inputs.add(inp)
-            zero_baseline, nonzero_baseline = calc_zero_and_nonzero_baselines(nonzero_inputs)
-         #   print_baselines()
+            #zero_baseline, nonzero_baseline = calc_zero_and_nonzero_baselines(nonzero_inputs)
+            #print_baselines()
         #print self.correlator.feng_eq_get().items()
